d3d656a76b478885ad43afd598a9af68ebf3b399
MinhajAhmed27 <60568039+MinhajAhmed27@users.noreply.github.com>
2021-01-27 14:47:18 +0500

    Add http call with OkHttp

--- app/src/main/java/com/example/twittercloneapp/Register.kt
+++ app/src/main/java/com/example/twittercloneapp/Register.kt
@@ -1,27 +1,26 @@
 package com.example.twittercloneapp
 
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.graphics.ImageDecoder
 import android.graphics.drawable.BitmapDrawable
 import android.net.Uri
+import android.os.AsyncTask
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.annotation.RequiresApi
-import androidx.core.app.ActivityCompat
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.ktx.storage
 import kotlinx.android.synthetic.main.activity_register.*
+import okhttp3.*
+import java.io.IOException
 import java.util.*
 
 class Register : AppCompatActivity() {
@@ -34,7 +33,7 @@ class Register : AppCompatActivity() {
         setContentView(R.layout.activity_register)
 
 //        auth = Firebase.auth
-        auth =FirebaseAuth.getInstance()
+        auth = FirebaseAuth.getInstance()
         storage = Firebase.storage
 
         signInAnonymously()
@@ -45,12 +44,13 @@ class Register : AppCompatActivity() {
         }
         register.setOnClickListener {
             performRegister()
+
         }
 
     }
 
 
-    fun register_btn(view: View){
+    fun register_btn(view: View) {
         signInAnonymously()
     }
 
@@ -72,13 +72,13 @@ class Register : AppCompatActivity() {
     }
 
 
-    val PICK_IMAGE_CODE=123
-    fun loadImage(){
+    val PICK_IMAGE_CODE = 123
+    fun loadImage() {
         // this should be call on setOnclickListener
         // open the file to view images
-        var intent= Intent(Intent.ACTION_PICK)
-        intent.type="image/*"
-        startActivityForResult(intent,PICK_IMAGE_CODE)
+        var intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, PICK_IMAGE_CODE)
         // after startActivityForResult(), onActivityResult override fun automatically call
     }
 
@@ -102,17 +102,17 @@ class Register : AppCompatActivity() {
     // this always call outside of onCreate because of override fun
     // allow to select the image
 
-    var selectedPhotoUri:Uri? = null
+    var selectedPhotoUri: Uri? = null
 
     @RequiresApi(Build.VERSION_CODES.P)
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if(requestCode==PICK_IMAGE_CODE  && data!=null && resultCode == RESULT_OK){
+        if (requestCode == PICK_IMAGE_CODE && data != null && resultCode == RESULT_OK) {
             // proceed & check what image was selected!
 
             // uri represents the location where that image is stored
-            selectedPhotoUri=data.data
+            selectedPhotoUri = data.data
             // receive the bitmap form getCapturedImage()
             val bitmap = getCapturedImage(selectedPhotoUri!!)
             // change it into drawable
@@ -124,7 +124,7 @@ class Register : AppCompatActivity() {
     }
 
 
-    fun performRegister(){
+    fun performRegister() {
         FirebaseAuth.getInstance().signInAnonymously().addOnCompleteListener {
             if (!it.isSuccessful) return@addOnCompleteListener
 
@@ -141,22 +141,50 @@ class Register : AppCompatActivity() {
         // creating a img filename which should be unique or random
         // here filename is consider as a unique id of img
         val filename = UUID.randomUUID().toString()
-        val ref=FirebaseStorage.getInstance().getReference("/images/$filename")
+        val ref = FirebaseStorage.getInstance().getReference("/images/$filename")
 
         ref.putFile(selectedPhotoUri!!)
             .addOnSuccessListener {
-                Log.d("Register","Successfully image is uploaded: ${it.metadata?.path}")
-
+                Log.d("Register", "Successfully image is uploaded: ${it.metadata?.path}")
+                Toast.makeText(this, "Image is successfully uploaded", Toast.LENGTH_LONG).show()
                 //if download URL needed
                 ref.downloadUrl.addOnSuccessListener {
-                    Log.d("Register","File location :$it")
+                    Log.d("Register", "File location :$it")
                     // Save img to some database
+                    // make http
+                    var url = "http://10.0.2.2/Twitter%20Android%20Clone/TwitterAndroidServer/register.php?first_name=" + username_reg.text + "&email=" + email_reg.text + "&password=" + password_reg.text + "&picture_path=" +it
+//                    var url = "http://10.0.2.2/Twitter%20Android%20Clone/TwitterAndroidServer/register.php?first_name=Faheem&email=h@yaho.com&password=123&picture_path=home"
+                    //        for background process
+                    httpCall(url)
                 }
             }
             .addOnFailureListener {
                 Log.d("Register", "Cant upload the image")
             }
     }
+
+    private fun httpCall(url:String) {
+        println("attempt to call http")
+
+        val request=Request.Builder().url(url).build();
+
+        val client = OkHttpClient()
+        client.newCall(request).enqueue(object: Callback{
+            override fun onFailure(call: Call, e: IOException) {
+                println("fail to call url: $e")
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val body = response.body?.string()
+                println(body)
+
+            }
+
+        })
+
+    }
+
+    // Call HTTP
 
 
 }
